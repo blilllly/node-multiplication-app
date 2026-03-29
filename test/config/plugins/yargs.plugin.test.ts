0bfec3405fb7c6b252876dfe0989ef5d74960f06
Billy Alvear <alvearbilly@gmail.com>
2026-03-29 16:14:47 -0500

    test yargs

--- test/config/plugins/yargs.plugin.test.ts
+++ test/config/plugins/yargs.plugin.test.ts
@@ -1,5 +1,3 @@
-const originalArgv = process.argv;
-
 const runCommand = async (args: string[]) => {
   process.argv = ['node', 'app.ts', ...args];
 
@@ -9,7 +7,9 @@ const runCommand = async (args: string[]) => {
 };
 
 describe('TestArgsPlugin', () => {
-  afterEach(() => {
+  const originalArgv = process.argv;
+
+  beforeEach(() => {
     process.argv = originalArgv;
     jest.resetModules();
   });
@@ -17,6 +17,39 @@ describe('TestArgsPlugin', () => {
   it('should return true', async () => {
     const argv = await runCommand(['-b', '5']);
 
-    expect(argv.b).toBe(5);
+    expect(argv).toEqual(
+      expect.objectContaining({
+        b: 5,
+        l: 10,
+        s: false,
+        n: 'multiplication-table',
+        d: 'outputs',
+      }),
+    );
+  });
+
+  it('shoul return configuration with custom values', async () => {
+    const argv = await runCommand([
+      '-b',
+      '7',
+      '-l',
+      '20',
+      '-s',
+      'true',
+      '-n',
+      'test-custom-n',
+      '-d',
+      'test-custom-d',
+    ]);
+
+    expect(argv).toEqual(
+      expect.objectContaining({
+        b: 7,
+        l: 20,
+        s: true,
+        n: 'test-custom-n',
+        d: 'test-custom-d',
+      }),
+    );
   });
 });
